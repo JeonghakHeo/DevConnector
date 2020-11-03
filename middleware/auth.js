b70f15c1708289ca796724909f2683c77588cc1c
Charlie <huh4142@naver.com>
2020-11-03 11:44:14 +1100

    lesson-13 Custom Auth Middleware & JWT Verify

---
+++ middleware/auth.js
@@ -0,0 +1,28 @@
+const jwt = require('jsonwebtoken');
+const config = require('config');
+
+module.exports = function (req, res, next) {
+  // Get token from header
+  const token = req.header('x-auth-token');
+
+  // Check if no token
+  if (!token) {
+    return res.status(401).json({ msg: 'No token, authorization denide' }); // 401 means not authorized
+  }
+
+  // Verify token
+  try {
+    const decoded = jwt.verify(token, config.get('jwtSecret'));
+
+    req.user = decoded.user;
+    // console.log(req.user, decoded); <- check for auth.js 
+    // decoded = user: { id: '5fa09b24af87fd1d36cc00b8' },
+    //                   iat: 1604360996,
+    //                   exp: 1604720996 
+    //                 }
+    next();
+  }
+  catch (err) {
+    res.status(401).json({ msg: 'token is not valid' })
+  }
+};
--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,10 +1,24 @@
 const express = require('express');
 const router = express.Router();
+const auth = require('../../middleware/auth');
+const User = require('../../models/User');
 
 // @route   GET api/auth
 // @desc    Test route
 // @access  Public 
-router.get('/', (req, res) => res.send('Auth route'));
+
+// middleware applied to protect route by adding auth before async (req, res) => {} 
+router.get('/', auth, async (req, res) => {
+  try {
+    //                                     get user info without password
+    const user = await User.findById(req.user.id).select('-password');
+    res.json(user);
+  }
+  catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server error')
+  }
+});
 
 module.exports = router;
 
