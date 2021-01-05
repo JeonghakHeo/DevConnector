cd774b81b02566d4244ebd962f2c46cee64bef76
Charlie <huh4142@naver.com>
2021-01-05 16:41:42 +1100

    lesson-41 Load User & Set Auth Token

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,9 +1,9 @@
 // rootReducer
 import { combineReducers } from 'redux'
 import alert from './alert';
-import register from './auth'
+import auth from './auth'
 
 export default combineReducers({
   alert,
-  register
+  auth
 });
--- client/src/store.js
+++ client/src/store.js
@@ -1,5 +1,4 @@
 // boiler plate
-
 import { createStore, applyMiddleware } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import thunk from 'redux-thunk';
