235de91dbeee2e0114c6f5a09dd912003516adc5
Charlie <huh4142@naver.com>
2021-01-07 13:45:34 +1100

    lesson-46 Starting On The Dashboard

--- client/src/components/layout/Spinner.js
+++ client/src/components/layout/Spinner.js
@@ -1,12 +1,10 @@
 import React, { Fragment } from 'react'
 import spinner from './spinner.gif'
 
-const Spinner = () => {
-  return (
-    <Fragment>
-      <img src={spinner} style={{ width: '200px', margin: 'auto', display: 'block' }} alt='loading...' />
-    </Fragment>
-  )
-}
+const Spinner = () => (
+  <Fragment>
+    <img src={spinner} style={{ width: '200px', margin: 'auto', display: 'block' }} alt='loading...' />
+  </Fragment>
+)
 
 export default Spinner
