2184b3c99c55660260c1f9f9a799c67500700a7b
Charlie <huh4142@naver.com>
2021-01-09 12:04:46 +1100

    lesson-52 Delete Educaiton, Experiences & Account

--- client/src/reducers/auth.js
+++ client/src/reducers/auth.js
@@ -5,7 +5,8 @@ import {
   AUTH_ERROR,
   LOGIN_SUCCESS,
   LOGIN_FAIL,
-  LOGOUT
+  LOGOUT,
+  ACCOUNT_DELETED
 } from '../actions/types';
 
 const initialState = {
@@ -42,6 +43,7 @@ const auth = (state = initialState, action) => {
     case REGISTER_FAIL:
     case AUTH_ERROR:
     case LOGOUT:
+    case ACCOUNT_DELETED:
       localStorage.removeItem('token');
       return {
         ...state,
